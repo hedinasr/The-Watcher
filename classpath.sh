45fc614a62e1e86ef7593b78f19415c0a10d74ec
Hedi Nasr <h.nsr69@gmail.com>
2016-05-30 20:50:37 +0200

    add JADE lib & update classpath.sh + gitignore

--- classpath.sh
+++ classpath.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export CLASSPATH=$CLASSPATH:/Volumes/Transcend/files/master/M1/MIF24/Jade/jade/lib/jade.jar:/Volumes/Transcend/files/master/M1/MIF24/Jade/jade/lib/jadeExamples.jar:/Volumes/Transcend/files/master/M1/MIF24/Jade/jade/lib/commons-codec/commons-codec-1.3.jar
+export CLASSPATH=$CLASSPATH:~/lib/jade.jar:~/lib/jadeExamples.jar:~/lib/commons-codec/commons-codec-1.3.jar
