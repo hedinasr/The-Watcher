9560316e2f2499c9b5a5194630e69b496325f314
Hedi Nasr <h.nsr69@gmail.com>
2016-05-31 11:47:38 +0200

    update classpath.sh

--- script/classpath.sh
+++ script/classpath.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export CLASSPATH=$CLASSPATH:../lib/jade.jar:../lib/jadeExamples.jar:../lib/commons-codec/commons-codec-1.3.jar
+export CLASSPATH=$CLASSPATH:$(pwd)/../lib/jade.jar:$(pwd)/../lib/jadeExamples.jar:$(pwd)../lib/commons-codec/commons-codec-1.3.jar
